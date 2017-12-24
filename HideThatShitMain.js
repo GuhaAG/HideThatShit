163c3a4427da4727f9f0398787d8f1d6db9780f6
“guhaAG” <guha.agt@gmail.com>
2017-12-24 16:22:33 +0900

    initial commit

---
+++ HideThatShitMain.js
@@ -0,0 +1,88 @@
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    
+    var tab = tabs[0];
+    var url = tab.url;
+
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+}
+
+/**
+ * Hide all occurences of keyword from content
+ *
+ * @param {string} work The keyword to hide content
+ */
+function hideContent(word) {
+  document.body.style.background = "red";
+  //var script = 'alert("hide all ' + work + '");';
+  
+  //chrome.tabs.executeScript({
+    //code: script
+  //});
+}
+
+/**
+ * Gets the saved keyword .
+ *
+ * @param {string} url URL whose hide keyword is to be retreived
+ * @param {function(string)} callback called with the hide keyword for
+ *     the given url on success, or a falsy value if no keyword is retrieved.
+ */
+function getSavedHideKeyword(url, callback) {
+  chrome.storage.sync.get(url, (items) => {
+    callback(chrome.runtime.lastError ? null : items[url]);
+  });
+}
+
+/**
+ * Saves the given keyword for url.
+ *
+ * @param {string} url URL for which keyword is to be saved.
+ * @param {string} keyword The keyword to be saved.
+ */
+function saveHideKeyword(url, word) {
+  var items = {};
+  items[url] = word;
+  
+  chrome.storage.sync.set(items);
+}
+
+// This extension loads the saved keyword for the current tab if one
+// exists. The user can enter a new keyword for the
+// current page.
+document.addEventListener('DOMContentLoaded', () => {
+  getCurrentTabUrl((url) => {
+    var keyword = document.getElementById('keyword');
+
+    // Load the saved keyword for this page and modify the input value if needed
+    getSavedHideKeyword(url, (saveHideKeyword) => {
+      if (saveHideKeyword) {
+        hideContent(saveHideKeyword);
+        keyword.value = saveHideKeyword;
+      }
+    });
+
+    var hideBtn = document.getElementById('hideBtn');
+
+    // Ensure the keyword is changed and saved when the input changes
+    hideBtn.addEventListener('click', () => {
+      hideContent(keyword.value);
+      saveHideKeyword(url, keyword.value);
+    });
+  });
+});
